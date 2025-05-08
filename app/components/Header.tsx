37e0809235f09e9cf7cb0bc0de0cecc3237a9b26
barnyj <barnyj@jkbweb.design>
2025-05-08 11:43:18 -0400

    save local changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -25,7 +25,7 @@ export default function Header() {
             <span className="sr-only">Barny's Web Builders</span>
             <img
               className="h-8 w-auto"
-              src="../../public/BWB-Logo.png"
+              src="/BWB-Logo.png"
               alt="Barny's Web Builders logo"
             />
           </Link>
