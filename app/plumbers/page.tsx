9b57ddbcc0a76a218fecb6513f5d3e26f56f338d
barnyj <barnyj@jkbweb.design>
2025-07-23 13:21:23 -0400

    fix booking link to calendly

--- app/plumbers/page.tsx
+++ app/plumbers/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
