bef404fa8f9c49a1aa8db05a8076d256f5f1788b
barnyj <barnyj@jkbweb.design>
2025-07-23 14:59:07 -0400

    fix: removed alt contact form

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,6 @@ export default function Home() {
       <PortfolioGrid />
       <Timeline />
       <Marquee />
-      <ContactForm />
       <NewsletterSubscribe />
       <ChatWidget />
     </>
